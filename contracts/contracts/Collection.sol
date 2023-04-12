a171afceb676415b65d916d99057d7a7416e2452
dmitry <strukovsky1@gmail.com>
2023-04-12 17:49:23 +0300

    feat: mock contracts

---
+++ contracts/contracts/Collection.sol
@@ -0,0 +1,27 @@
+//SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+contract Collection is ERC721Enumerable, AccessControl {
+
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+    // unique role related to SaleContract which can mint tokens on buying them 
+    bytes32 public constant SALE_CONTRACT_ROLE = keccak256("SALE_CONTRACT_ROLE");
+
+    constructor(string memory name, string memory symbol) ERC721(name, symbol) {
+        _grantRole(MINTER_ROLE, _msgSender());
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721Enumerable, AccessControl) returns (bool) {
+        return interfaceId == type(IERC721Enumerable).interfaceId && interfaceId == type(AccessControl).interfaceId;
+    }
+
+    function mint(address to, uint256 id) public onlyRole(MINTER_ROLE) {
+        _mint(to, id);
+    }
+
+}
---
+++ contracts/contracts/GovernanceToken__mock.sol
@@ -0,0 +1,19 @@
+//SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+contract GovernanceToken is ERC20, AccessControl {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+    constructor() ERC20("Name", "TCKR") {
+        _grantRole(DEFAULT_ADMIN_ROLE, _msgSender());
+        _grantRole(MINTER_ROLE, _msgSender());
+    }
+
+    function mint(address to, uint256 value) public onlyRole(MINTER_ROLE) {
+        _mint(to, value);
+    }
+}
