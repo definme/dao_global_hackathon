0554482a1a273035f00862ef0353659cea87d0dd
Tanya <t.kozlova@ongrid.pro>
2023-04-13 11:01:50 +0300

    feat: add DAO page

---
+++ app/src/pages/DAO/index.jsx
@@ -0,0 +1,48 @@
+import Container from '@mui/material/Container'
+import Typography from '@mui/material/Typography'
+import Box from '@mui/material/Box'
+
+function DAO() {
+  return (
+    <Container sx={{ mb: '40px' }}>
+      <Box>
+        <Typography
+          variant='h4'
+          component='h1'
+          gutterBottom
+          sx={{ m: '40px auto 60px', textAlign: 'center', fontWeight: 700 }}
+        >
+          EtherLuxe DAO
+        </Typography>
+        <Box
+          sx={{
+            width: '100%',
+            display: 'flex',
+            flexWrap: 'wrap',
+            gap: '20px',
+            justifyContent: 'center',
+            mb: '40px',
+          }}
+        >
+          <Typography
+            variant='p'
+            component='p'
+            gutterBottom
+            sx={{ m: '10px', textAlign: 'center', fontWeight: 700 }}
+          >
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi non
+            magnam perspiciatis perferendis veniam nam, aut alias ab debitis
+            fugiat. Esse assumenda, aliquam eum quos ducimus perspiciatis
+            similique temporibus tempora! Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Delectus, repudiandae velit voluptatem iusto
+            magnam, repellat deserunt dolorum provident blanditiis quasi
+            laudantium. Vel, libero. Obcaecati minus ut corporis ipsa! Impedit,
+            laborum!
+          </Typography>
+        </Box>
+      </Box>
+    </Container>
+  )
+}
+
+export default DAO
