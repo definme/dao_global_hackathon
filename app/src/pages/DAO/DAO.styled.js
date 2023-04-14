ed10f5b4760b1944f36993763472d408b55d61d1
Tanya <t.kozlova@ongrid.pro>
2023-04-14 10:32:53 +0300

    style: display dao info

---
+++ app/src/pages/DAO/DAO.styled.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const DAOLink = styled.a`
+  color: lightseagreen;
+`
+
+export const DAOAddressLink = styled.a`
+  color: lightseagreen;
+  text-decoration: none;
+`
--- app/src/pages/DAO/index.jsx
+++ app/src/pages/DAO/index.jsx
@@ -3,49 +3,92 @@ import Container from '@mui/material/Container'
 import Typography from '@mui/material/Typography'
 import Box from '@mui/material/Box'
 import { ConnectionContext } from '../../contexts/ConnectionContext'
+import networks from '../../networks.json'
+import { APP_NETWORK } from '../../constants'
+import { DAOLink, DAOAddressLink } from './DAO.styled'
 
 function DAO() {
   const { dao } = useContext(ConnectionContext)
 
-  console.log(dao)
+  function getIPFSLink(ipfs) {
+    return 'https://ipfs.eth.aragon.network/ipfs/' + ipfs.split('//')[1]
+  }
+
   return (
     <Container sx={{ mb: '40px' }}>
-      <Box>
-        <Typography
-          variant='h4'
-          component='h1'
-          gutterBottom
-          sx={{ m: '40px auto 60px', textAlign: 'center', fontWeight: 700 }}
-        >
-          EtherLuxe DAO
-        </Typography>
+      {dao && (
         <Box
           sx={{
             width: '100%',
+            boxSizing: 'border-box',
             display: 'flex',
-            flexWrap: 'wrap',
             gap: '20px',
             justifyContent: 'center',
-            mb: '40px',
+            flexDirection: 'column',
+            background: 'rgba(0, 0, 0, 0.8);',
+            borderRadius: '20px',
+            m: '40px auto 60px',
+            p: '40px',
           }}
         >
-          <Typography
-            variant='p'
-            component='p'
-            gutterBottom
-            sx={{ m: '10px', textAlign: 'center', fontWeight: 700 }}
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+            }}
           >
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi non
-            magnam perspiciatis perferendis veniam nam, aut alias ab debitis
-            fugiat. Esse assumenda, aliquam eum quos ducimus perspiciatis
-            similique temporibus tempora! Lorem ipsum dolor sit amet consectetur
-            adipisicing elit. Delectus, repudiandae velit voluptatem iusto
-            magnam, repellat deserunt dolorum provident blanditiis quasi
-            laudantium. Vel, libero. Obcaecati minus ut corporis ipsa! Impedit,
-            laborum!
-          </Typography>
+            <Typography
+              variant='h4'
+              component='h1'
+              gutterBottom
+              sx={{
+                textAlign: 'center',
+                fontWeight: 700,
+                color: 'lightseagreen',
+              }}
+            >
+              {dao.dao?.metadata?.name}
+            </Typography>
+            <DAOAddressLink
+              href={
+                networks[APP_NETWORK].params.blockExplorerUrls[0] +
+                'address/' +
+                dao.dao?.address
+              }
+              target='_blank'
+            >
+              {dao.dao?.address}
+            </DAOAddressLink>
+          </Box>
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              alignItems: 'flex-start',
+              gap: '20px',
+            }}
+          >
+            <Box sx={{ maxWidth: '800px' }}>
+              <Typography
+                variant='p'
+                component='p'
+                gutterBottom
+                sx={{
+                  color: 'white',
+                  mb: '40px',
+                }}
+              >
+                {dao.dao?.metadata?.description}
+              </Typography>
+              <DAOLink href={dao.dao?.metadata?.links[0].url} target='_blank'>
+                {dao.dao?.metadata?.links[0].url}
+              </DAOLink>
+            </Box>
+            <img src={getIPFSLink(dao.dao?.metadata?.avatar)} alt='avatar' />
+          </Box>
         </Box>
-      </Box>
+      )}
     </Container>
   )
 }
