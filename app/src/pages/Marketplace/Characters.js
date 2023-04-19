b8f3ab0a7227038568c08b499d64ae34ede7bc8f
Tanya <t.kozlova@ongrid.pro>
2023-04-19 11:21:15 +0300

    feat: get nft price and buy nft request

--- app/src/pages/Marketplace/Characters.js
+++ app/src/pages/Marketplace/Characters.js
@@ -1,7 +1,29 @@
+import { useState, useEffect } from 'react'
 import Box from '@mui/material/Box'
 import BuyCard from '../../components/BuyCard'
+import networks from '../../networks.json'
+import { APP_NETWORK } from '../../constants'
+import { getCollectionSale } from '../../api/contracts'
 
 function Characters() {
+  const [price, setPrice] = useState()
+
+  async function getPrice() {
+    const CollectionSale = getCollectionSale()
+
+    await CollectionSale.getPrice(
+      networks[APP_NETWORK].contracts.charactersCollection
+    )
+      .then(res => {
+        setPrice(res)
+      })
+      .catch(e => console.log(e))
+  }
+
+  useEffect(() => {
+    getPrice()
+  }, [])
+
   return (
     <Box
       sx={{
@@ -15,20 +37,32 @@ function Characters() {
       <BuyCard
         title='Character'
         description='Some Description'
-        price='100'
         image={require('../../images/char.png')}
+        collectionContract={
+          networks[APP_NETWORK].contracts.charactersCollection
+        }
+        kind={0x0000}
+        price={price}
       />
       <BuyCard
         title='Character'
         description='Some Description'
-        price='100'
+        price={price}
         image={require('../../images/char.png')}
+        collectionContract={
+          networks[APP_NETWORK].contracts.charactersCollection
+        }
+        kind={0x0000}
       />
       <BuyCard
         title='Character'
         description='Some Description'
-        price='100'
+        price={price}
         image={require('../../images/char.png')}
+        collectionContract={
+          networks[APP_NETWORK].contracts.charactersCollection
+        }
+        kind={0x0000}
       />
     </Box>
   )
--- app/src/pages/Marketplace/Weapon.js
+++ app/src/pages/Marketplace/Weapon.js
@@ -1,7 +1,29 @@
+import { useState, useEffect } from 'react'
 import Box from '@mui/material/Box'
 import BuyCard from '../../components/BuyCard'
+import networks from '../../networks.json'
+import { APP_NETWORK } from '../../constants'
+import { getCollectionSale } from '../../api/contracts'
 
 function Weapon() {
+  const [price, setPrice] = useState()
+
+  async function getPrice() {
+    const CollectionSale = getCollectionSale()
+
+    await CollectionSale.getPrice(
+      networks[APP_NETWORK].contracts.charactersCollection
+    )
+      .then(res => {
+        setPrice(res)
+      })
+      .catch(e => console.log(e))
+  }
+
+  useEffect(() => {
+    getPrice()
+  }, [])
+
   return (
     <Box
       sx={{
@@ -15,20 +37,26 @@ function Weapon() {
       <BuyCard
         title='Weapon'
         description='Some Description'
-        price='20'
+        price={price}
         image={require('../../images/weapon.png')}
+        collectionContract={networks[APP_NETWORK].contracts.weaponCollection}
+        kind={0x0000}
       />
       <BuyCard
         title='Weapon'
         description='Some Description'
-        price='20'
+        price={price}
         image={require('../../images/weapon.png')}
+        collectionContract={networks[APP_NETWORK].contracts.weaponCollection}
+        kind={0x0000}
       />
       <BuyCard
         title='Weapon'
         description='Some Description'
-        price='20'
+        price={price}
         image={require('../../images/weapon.png')}
+        collectionContract={networks[APP_NETWORK].contracts.weaponCollection}
+        kind={0x0000}
       />
     </Box>
   )
