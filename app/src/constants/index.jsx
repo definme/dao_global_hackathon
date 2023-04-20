c703e19cb1cd205d03b4ad2bf4c14b817eeeabf7
Tanya <t.kozlova@ongrid.pro>
2023-04-20 16:24:09 +0300

    feat: get nfts from api

--- app/src/constants/index.jsx
+++ app/src/constants/index.jsx
@@ -2,3 +2,5 @@ import networks from '../networks.json'
 
 export const APP_NETWORK = Object.keys(networks)[0]
 export const IPFS_API_KEY = process.env.REACT_APP_IPFS_API_KEY
+
+export const API_HOST = 'https://api-ether-luxe.definme.com/api'
