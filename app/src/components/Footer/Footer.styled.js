43c1639862145ef2d467a2ea93f49cac6d4077aa
Tanya <t.kozlova@ongrid.pro>
2023-04-24 16:42:42 +0300

    style: footer styles

---
+++ app/src/components/Footer/Footer.styled.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+export const FooterText = styled.p`
+  margin: 0;
+  font-family: Open Sans;
+  font-size: 17px;
+  font-weight: 400;
+  line-height: 23px;
+  letter-spacing: 0.01em;
+  text-align: center;
+  color: #ffffff;
+`
+
+export const FooterLink = styled.a`
+  color: #ffffff;
+`
