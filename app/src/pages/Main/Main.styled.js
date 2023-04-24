a8cfed7097c92f13b41e299bf3699eb7e732164a
Tanya <t.kozlova@ongrid.pro>
2023-04-24 17:26:33 +0300

    style: main page styles

---
+++ app/src/pages/Main/Main.styled.js
@@ -0,0 +1,176 @@
+import styled from 'styled-components'
+
+export const MainContainer = styled.div`
+  max-width: 840px;
+  margin: 70px auto 120px;
+  width: 100%;
+`
+
+export const MainTitleContainer = styled.div`
+  display: flex;
+  gap: 40px;
+  align-items: center;
+  margin-bottom: 65px;
+`
+
+export const MainTitle = styled.h1`
+  font-family: Open Sans;
+  font-size: 40px;
+  font-weight: 700;
+  line-height: 54px;
+  letter-spacing: 0.01em;
+  text-align: left;
+  color: #ffffff;
+  margin: 0;
+`
+
+export const MainTitleSpan = styled.span`
+  font-weight: 300;
+`
+
+export const MainTitleWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+`
+
+export const MainSubtitle = styled.p`
+  font-family: Open Sans;
+  font-size: 17px;
+  font-weight: 400;
+  line-height: 26px;
+  letter-spacing: 0.01em;
+  text-align: left;
+  color: #e2e2e2;
+  margin: 0;
+  max-width: 514px;
+`
+
+export const MainText = styled.p`
+  font-family: Open Sans;
+  font-size: 17px;
+  font-weight: 400;
+  line-height: 26px;
+  letter-spacing: 0.01em;
+  text-align: left;
+  color: #e2e2e2;
+  margin-bottom: 23px;
+`
+
+export const MainRules = styled.p`
+  font-family: Open Sans;
+  font-size: 17px;
+  font-weight: 400;
+  line-height: 26px;
+  letter-spacing: 0.01em;
+  text-align: left;
+  color: #e2e2e2;
+  background: #22222e;
+  padding: 15px 20px;
+  width: max-content;
+  border-radius: 2px;
+  margin: 0;
+  margin-bottom: 48px;
+`
+
+export const MainRulesSpan = styled.span`
+  font-weight: 700;
+`
+
+export const MainDistributionTitle = styled.h2`
+  font-family: Open Sans;
+  font-size: 20px;
+  font-weight: 600;
+  line-height: 30px;
+  letter-spacing: 0.01em;
+  text-align: left;
+  color: #ffffff;
+  margin: 0;
+  margin-bottom: 18px;
+`
+
+export const MainTable = styled.div`
+  border: 1px solid rgba(255, 255, 255, 0.1);
+  border-radius: 12px;
+  overflow: hidden;
+`
+
+export const MainTableHeader = styled.div`
+  background: #22222e;
+  padding: 12px 0;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+`
+
+export const MainTableBody = styled.div`
+  color: #ffffff;
+`
+
+export const TableHeaderItem = styled.div`
+  position: relative;
+  padding: 14px;
+  font-family: Open Sans;
+  font-size: 17px;
+  font-weight: 400;
+  line-height: 27px;
+  letter-spacing: 0.01em;
+  text-align: center;
+  color: #ffffff;
+  &:before {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    top: 0;
+    width: ${({ first }) => (first ? '0px' : '1px')};
+    background-color: rgba(255, 255, 255, 0.1);
+  }
+`
+
+export const TableBodyItem = styled.div`
+  padding: 16px;
+  position: relative;
+  font-family: Open Sans;
+  font-size: 17px;
+  font-weight: 400;
+  line-height: 22px;
+  letter-spacing: 0.01em;
+  text-align: left;
+
+  &:before {
+    content: '';
+    position: absolute;
+    bottom: ${({ bottom }) => (bottom ? '10px' : '0px')};
+    left: 0;
+    top: ${({ top }) => (top ? '10px' : '0px')};
+    width: ${({ first }) => (first ? '0px' : '1px')};
+    background-color: rgba(255, 255, 255, 0.1);
+  }
+  &:after {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    height: ${({ withoutAfter }) => (withoutAfter ? '0px' : '1px')};
+    background-color: rgba(255, 255, 255, 0.1);
+  }
+`
+
+export const MainTableBodyRow = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+`
+
+export const TableBodySpan = styled.span`
+  font-weight: 700;
+`
+
+export const TableIndicator = styled.span`
+  display: inline-block;
+  background: #ff6933;
+  border-radius: 50%;
+  width: 11px;
+  height: 11px;
+  margin-right: 8px;
+`
--- app/src/pages/Main/index.jsx
+++ app/src/pages/Main/index.jsx
@@ -1,100 +1,118 @@
-import Container from '@mui/material/Container'
-import Typography from '@mui/material/Typography'
-import Box from '@mui/material/Box'
+import {
+  MainContainer,
+  MainTitleContainer,
+  MainTitle,
+  MainTitleSpan,
+  MainTitleWrapper,
+  MainSubtitle,
+  MainText,
+  MainRules,
+  MainRulesSpan,
+  MainDistributionTitle,
+  MainTable,
+  MainTableHeader,
+  MainTableBody,
+  TableBodyItem,
+  TableHeaderItem,
+  MainTableBodyRow,
+  TableBodySpan,
+  TableIndicator,
+} from './Main.styled'
 
 function Main() {
   return (
-    <Container sx={{ mb: '40px' }}>
-      <Box>
-        <Typography
-          variant='h4'
-          component='h1'
-          gutterBottom
-          sx={{
-            m: '40px auto 60px',
-            textAlign: 'center',
-            fontWeight: 700,
-            color: '#FFFFFF',
-          }}
-        >
-          EtherLuxe NFTxDAO
-        </Typography>
-        <Box
-          sx={{
-            width: '80%',
-            display: 'flex',
-            flexWrap: 'wrap',
-            gap: '20px',
-            justifyContent: 'center',
-            m: 'auto',
-            mb: '40px',
-          }}
-        >
-          <Typography
-            variant='p'
-            component='p'
-            gutterBottom
-            sx={{ fontWeight: 700, color: '#E2E2E2' }}
-          >
+    <MainContainer>
+      <MainTitleContainer>
+        <img
+          src={require('../../images/Logo.png')}
+          alt='logo'
+          width='126px'
+          height='184px'
+        />
+        <MainTitleWrapper>
+          <MainTitle>
+            Ether<MainTitleSpan>Luxe</MainTitleSpan> NFTxDAO
+          </MainTitle>
+          <MainSubtitle>
             Introducing EtherLuxe DAO, allowing players to influence the further
             development of the game!
-          </Typography>
-          <Typography
-            variant='p'
-            component='p'
-            gutterBottom
-            sx={{ fontWeight: 500, lineHeight: '24px', color: '#E2E2E2' }}
-          >
-            To achieve our goals, we used Aragon DAO and ERC721 game tokens. The
-            principle of our project is: <br />
-            <br />
-            - user buys a game character token on the "Marketplace" page using
-            the network's native tokens(MATIC) <br />
-            <br />- for the purchase of a character, user is credited with a
-            certain number of game governance tokens(ELT). The starting number
-            of tokens awarded for purchasing a character is 250 ELT. The more
-            characters sold, the less governance tokens will be credited for the
-            purchase - this is how we encourage our first users. The total
-            balance of governance tokens is 10000 ELT, before they are
-            distributed among users, they are stored on the "CollectionSale"
-            contract.
-            <br />
-            <br />- when 7000 ELT are distributed among users, we will enable
-            the ability to create promotions with suggestions for improvements
-            in the game.
-            <br />
-            <br />- if the proposal gets enough “yes” votes, then we will take
-            it into development and add it to the game!
-          </Typography>
-          <Typography
-            variant='p'
-            component='p'
-            gutterBottom
-            sx={{
-              textAlign: 'center',
-              fontWeight: 500,
-              lineHeight: '24px',
-              color: '#E2E2E2',
-            }}
-          >
-            Rules for taking proposal:
-          </Typography>
-          <Typography
-            variant='p'
-            component='p'
-            gutterBottom
-            sx={{
-              textAlign: 'center',
-              fontWeight: 500,
-              lineHeight: '24px',
-              color: '#E2E2E2',
-            }}
-          >
-            - 15% participants <br /> - 50% "yes" votes
-          </Typography>
-        </Box>
-      </Box>
-    </Container>
+          </MainSubtitle>
+        </MainTitleWrapper>
+      </MainTitleContainer>
+      <MainText>
+        To achieve our goals, we used Aragon DAO and ERC721 game tokens. The
+        principle of our project is: <br />
+        <br />
+        - user buys a game character token on the "Marketplace" page using the
+        network's native tokens(MATIC) <br />
+        <br />- for the purchase of a character, user is credited with a certain
+        number of game governance tokens(ELT). The starting number of tokens
+        awarded for purchasing a character is 250 ELT. The more characters sold,
+        the less governance tokens will be credited for the purchase - this is
+        how we encourage our first users. The total balance of governance tokens
+        is 10000 ELT, before they are distributed among users, they are stored
+        on the "CollectionSale" contract.
+        <br />
+        <br />- when 7000 ELT are distributed among users, we will enable the
+        ability to create promotions with suggestions for improvements in the
+        game.
+        <br />
+        <br />- if the proposal gets enough “yes” votes, then we will take it
+        into development and add it to the game!
+      </MainText>
+      <MainRules>
+        Rules for taking proposal:{' '}
+        <MainRulesSpan>- 15% participants, - 50% "yes" votes.</MainRulesSpan>
+      </MainRules>
+      <MainDistributionTitle>
+        Token Distribution Graphics:
+      </MainDistributionTitle>
+      <MainTable>
+        <MainTableHeader>
+          <TableHeaderItem first>
+            <TableIndicator />I stage
+          </TableHeaderItem>
+          <TableHeaderItem>II stage</TableHeaderItem>
+          <TableHeaderItem>III stage</TableHeaderItem>
+          <TableHeaderItem>IV stage</TableHeaderItem>
+        </MainTableHeader>
+        <MainTableBody>
+          <MainTableBodyRow>
+            <TableBodyItem first>
+              <TableBodySpan>10,000</TableBodySpan>
+              voting tokens
+            </TableBodyItem>
+            <TableBodyItem top>
+              <TableBodySpan>+5,000</TableBodySpan> voting tokens
+            </TableBodyItem>
+            <TableBodyItem top>
+              <TableBodySpan>+2,500</TableBodySpan> voting tokens
+            </TableBodyItem>
+            <TableBodyItem top>
+              <TableBodySpan>+1,000</TableBodySpan> voting tokens
+            </TableBodyItem>
+          </MainTableBodyRow>
+          <MainTableBodyRow>
+            <TableBodyItem first withoutAfter>
+              For <TableBodySpan>1</TableBodySpan> character you get{' '}
+              <TableBodySpan>25</TableBodySpan> tokens
+            </TableBodyItem>
+            <TableBodyItem bottom withoutAfter>
+              For <TableBodySpan>1</TableBodySpan> character you get{' '}
+              <TableBodySpan>15</TableBodySpan> tokens
+            </TableBodyItem>
+            <TableBodyItem bottom withoutAfter>
+              For <TableBodySpan>1</TableBodySpan> character you get{' '}
+              <TableBodySpan>10</TableBodySpan> tokens
+            </TableBodyItem>
+            <TableBodyItem bottom withoutAfter>
+              For <TableBodySpan>1</TableBodySpan> character you get{' '}
+              <TableBodySpan>5</TableBodySpan> tokens
+            </TableBodyItem>
+          </MainTableBodyRow>
+        </MainTableBody>
+      </MainTable>
+    </MainContainer>
   )
 }
 
