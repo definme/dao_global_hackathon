0702230108caf1a2f66fd77ce8ac2656d7df9908
Tanya <t.kozlova@ongrid.pro>
2023-04-24 16:42:42 +0300

    style: footer styles

--- app/src/components/Footer/index.jsx
+++ app/src/components/Footer/index.jsx
@@ -1,5 +1,5 @@
 import Box from '@mui/material/Box'
-import Typography from '@mui/material/Typography'
+import { FooterText, FooterLink } from './Footer.styled'
 
 export default function Footer() {
   return (
@@ -8,35 +8,23 @@ export default function Footer() {
         width: '100%',
         boxSizing: 'border-box',
         display: 'flex',
-        flexDirection: 'column',
+        justifyContent: 'center',
         alignItems: 'center',
-        background: 'rgba(0, 0, 0, 0.5)',
+        background: ' #22222E;',
         mt: 'auto',
-        p: '40px',
+        p: '30px',
       }}
     >
-      <a href='https://definme.com/' target='_blank' rel='noreferrer'>
-        <img
-          src={require('../../images/definme.svg').default}
-          alt='definmeLogo'
-          style={{
-            marginBottom: '10px',
-          }}
-        />
-      </a>
-      <Typography
-        sx={{
-          fontFamily: '"Inter", sans-serif',
-          fontStyle: 'normal',
-          fontWeight: '400',
-          fontSize: '18px',
-          lineHeight: '22px',
-          letterSpacing: '0.01em',
-          color: 'white',
-        }}
-      >
-        The project was developed with love by Definme.
-      </Typography>
+      <FooterText>
+        ©{new Date().getFullYear()} EtherLuxe. Was developed with love by{' '}
+        <FooterLink
+          href='https://definme.com/'
+          target='_blank'
+          rel='noreferrer'
+        >
+          Definme
+        </FooterLink>
+      </FooterText>
     </Box>
   )
 }
