4573c4b5517c6e8a68991fb7622aba20afa7ca24
Dmitry <d.suldin@ongrid.pro>
2023-04-28 17:35:42 +0400

    feat: add voters endpoint

--- api/ether_luxe/urls.py
+++ api/ether_luxe/urls.py
@@ -1,10 +1,11 @@
 from django.urls import include, path
 from rest_framework import routers
-from .views import TokenViewSet, SaleTokenViewSet
+from .views import TokenViewSet, SaleTokenViewSet, VotersViewSet
 
 router = routers.DefaultRouter()
 router.register(r'collection', TokenViewSet)
 router.register(r'sale', SaleTokenViewSet)
+router.register(r'voters', VotersViewSet, basename='voters')
 
 urlpatterns = [
     path('', include(router.urls)),
